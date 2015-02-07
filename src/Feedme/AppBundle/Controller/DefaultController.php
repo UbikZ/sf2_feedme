de503a9b2426d5dff843aeb26cd929d5951d58a0
Gabriel Malet <gmalet@galilee.fr>
2015-02-07 19:05:37 +0100

    Disable defaulting

--- src/Feedme/AppBundle/Controller/DefaultController.php
+++ src/Feedme/AppBundle/Controller/DefaultController.php
@@ -7,16 +7,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route(
-     *      "/{_locale}",
-     *      requirements={"_locale" = "en|fr"},
-     *      defaults={"_locale" = "en"},
-     *      name="homepage"
-     * )
-     */
-    public function indexAction()
-    {
-        return $this->render('default/index.html.twig');
-    }
 }
