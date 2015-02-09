0e141aadc2abe1ccca7e57351a8b7e7e634dea70
Gabriel Malet <gmalet@galilee.fr>
2015-02-09 11:33:58 +0100

    Add new route for Dashboard

--- src/Feedme/AppBundle/Controller/DefaultController.php
+++ src/Feedme/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,20 @@
 
 namespace Feedme\AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class DefaultController
+ * @package Feedme\AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function indexAction()
+    {
+        echo "dashboard";die;
+    }
 }
