406d6bbc5a2af6d808303db42ec3cf903b53d643
Gabriel Malet <gmalet@galilee.fr>
2015-02-10 15:40:37 +0100

    Add new fields for registration box

---
+++ src/Feedme/FeedmeUserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Feedme\FeedmeUserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use \FOS\UserBundle\Form\Type\RegistrationFormType as FOSUserRegistrationFormType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends FOSUserRegistrationFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('firstname', null,['constraints' => $this->getValidators('firstname'), 'translation_domain' => 'messages'])
+            ->add('lastname', null, ['constraints' => $this->getValidators('lastname'), 'translation_domain' => 'messages'])
+        ;
+    }
+
+    public function getName()
+    {
+        return 'feedme_user_registration';
+    }
+
+    private function getValidators($name)
+    {
+        $validators = [];
+        $validator = new NotBlank(['groups' => ['Registration', 'Profile']]);
+        $validator->message = sprintf('form.validation.%s.notblank', $name);
+        $validators[] = $validator;
+
+        return $validators;
+    }
+}
