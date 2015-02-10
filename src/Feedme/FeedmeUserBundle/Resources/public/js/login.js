75267951c785d56956156de0d39040355432cc93
Gabriel Malet <gmalet@galilee.fr>
2015-02-10 17:32:17 +0100

    Fix stuff and simplify javascript execution

--- src/Feedme/FeedmeUserBundle/Resources/public/js/login.js
+++ src/Feedme/FeedmeUserBundle/Resources/public/js/login.js
@@ -1,20 +1,17 @@
-var handleLoginPageChangeBackground = function() {
-    $('[data-click="change-bg"]').live("click", function() {
-        var e = '[data-id="login-cover-image"]';
-        var t = $(this).find("img").attr("src");
-        var n = '<img src="' + t + '" data-id="login-cover-image" />';
-        $(".login-cover-image").prepend(n);
-        $(e).not('[src="' + t + '"]').fadeOut("slow", function() {
-            $(this).remove()
-        });
-        $('[data-click="change-bg"]').closest("li").removeClass("active");
-        $(this).closest("li").addClass("active")
-    })
+var handleAfterPageLoadAddClass = function() {
+    if ($("[data-pageload-addclass]").length !== 0) {
+        $(window).load(function() {
+            $("[data-pageload-addclass]").each(function() {
+                var e = $(this).attr("data-pageload-addclass");
+                $(this).addClass(e)
+            })
+        })
+    }
 };
 
 var LoginV2 = function() {
     "use strict";
     return {init: function() {
-        handleLoginPageChangeBackground()
+        handleAfterPageLoadAddClass()
     }}
 }();
