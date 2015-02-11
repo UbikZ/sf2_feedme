3ceec1f86d67dab2ce63ba14edddeb336a12e396
Gabriel Malet <gmalet@galilee.fr>
2015-02-11 14:58:46 +0100

    Add default controller with ajax action

--- src/Feedme/AppBundle/Controller/DefaultController.php
+++ src/Feedme/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,11 @@ namespace Feedme\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class DefaultController
@@ -23,4 +28,22 @@ class DefaultController extends Controller
 
         return $this->render('AppBundle:Default:index.html.twig', ['user' => $this->getUser()]);
     }
+
+    /**
+     * @Route("/me", name="me")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function userAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            $this->createNotFoundException();
+        }
+
+        $serializer = new Serializer([new GetSetMethodNormalizer()], [new JsonEncoder()]);
+
+        return new JsonResponse($serializer->serialize($this->getUser(), 'json'));
+    }
+
+
 }
