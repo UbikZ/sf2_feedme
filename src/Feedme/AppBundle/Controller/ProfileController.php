d449384f878aa5245768a45a5aba02d36f9b442b
Gabriel Malet <gmalet@galilee.fr>
2015-02-11 15:02:50 +0100

    Add controller / form for profile edit

---
+++ src/Feedme/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Feedme\AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class ProfileController
+ * @package Feedme\AppBundle\Controller
+ */
+class ProfileController extends Controller
+{
+}
---
+++ src/Feedme/FeedmeUserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Feedme\FeedmeUserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as FOSUserProfileFormType;
+
+/**
+ * Class ProfileFormType
+ * @package Feedme\FeedmeUserBundle\Form\Type
+ */
+class ProfileFormType extends FOSUserProfileFormType
+{
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('firstname', null, ['label' => 'form.firstname', 'required' => true])
+            ->add('lastname', null, ['label' => 'form.lastname', 'required' => true])
+            ->add('organization', null, ['label' => 'form.organization', 'required' => false])
+            ->add('comment', 'textarea', ['label' => 'form.comment', 'required' => false, 'attr' => ['rows' => 4, 'cols' => 49]])
+            ->add('location', null, ['label' => 'form.location', 'required' => false])
+            ->add('website', 'url', ['label' => 'form.website', 'required' => false])
+        ;
+    }
+
+    public function getName()
+    {
+        return 'feedme_user_profile';
+    }
+}
