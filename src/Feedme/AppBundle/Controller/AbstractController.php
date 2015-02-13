730db5a4a4404d800b1d5d0041cadfbd0810439d
Gabriel Malet <gmalet@galilee.fr>
2015-02-13 15:28:52 +0100

    Add abstract controller

---
+++ src/Feedme/AppBundle/Controller/AbstractController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Feedme\AppBundle\Controller;
+
+use Feedme\FeedmeUserBundle\Model\Message\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class DefaultController
+ * @package Feedme\AppBundle\Controller
+ */
+abstract class AbstractController extends Controller
+{
+}
