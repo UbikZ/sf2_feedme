3000e9bc6130484a53578186c7517e5ac936feec
Gabriel Malet <gmalet@galilee.fr>
2015-02-13 15:28:13 +0100

    Add new service manager for user to provite standard result

---
+++ src/Feedme/FeedmeUserBundle/Model/Service/Filter/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Feedme\FeedmeUserBundle\Model\Service\Filter;
+
+/**
+ * Class User
+ * @package Feedme\FeedmeUserBundle\Model\Service\Filter;
+ */
+class User
+{
+    /** @var  int */
+    public $id;
+    /** @var  string */
+    public $firstname;
+    /** @var  string */
+    public $lastname;
+}
